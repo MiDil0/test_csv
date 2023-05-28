7fd86b3b86a8eb39186ef664ae56b793ff248396
Mikle Zolotorev <shadoumaikl@gmail.com>
2023-05-29 00:21:46 +0300

    .

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import _ from 'lodash';
-
-export default function solution(content){
-  // BEGIN
-  console.log(content);
-  // END
+import _ from 'lodash'; 
+ 
+export default function solution(content){ 
+  // BEGIN 
+ const rows = content.trim().split('\r\n').slice(1) 
+ const data = rows.map((row) => row.split(',')) 
+ 
+ const averageAge = data.reduce((acc, passenger) => acc + Number(passenger[6]), 0) / data.length 
+  
+ const ports = data.reduce((acc, passenger) => { 
+  const count = _.get(acc, passenger[12], 0) + 1 
+  return { ...acc, [passenger[12]] : count} 
+}, {}) 
+console.log(`Ports: ${JSON.stringify(ports)}`) 
+  // END 
 }
